053526568ce446bfcd4d93fb9bf86cc5742e2b4d
JoachimPerrin <joachimperrin1@gmail.com>
2025-11-20 17:36:48 +0100

    add compiling template

--- src/my_teleop_joy/launch/my_teleop.launch.py
+++ src/my_teleop_joy/launch/my_teleop.launch.py
@@ -1,7 +1,6 @@
 import launch
 import launch_ros.actions
 
-
 def generate_launch_description():
     return launch.LaunchDescription([
         launch_ros.actions.Node(
@@ -10,13 +9,13 @@ def generate_launch_description():
             name='joystick'),
 
         launch_ros.actions.Node(
-            package='my_teleop',
-            executable='my_teleop_node',
-            remappings=["/cmd_vel", "turtle1/cmd_vel"],
+            package='my_teleop_joy',
+            executable='my_teleop_joy_node',
+            remappings=[("/cmd_vel", "turtle1/cmd_vel")],
             name='teleop'),
 
         launch_ros.actions.Node(
             package='turtlesim',
             executable='turtlesim_node',
-            name='turtle'),        
+            name='turtle'),
     ])
